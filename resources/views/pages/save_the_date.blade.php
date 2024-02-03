d5a4bf0cf72eb0f2f4f74d28381fbdebd59a9117
Joeri <joeri@digiti.be>
2024-02-03 22:07:26 +0100

    progress

--- resources/views/pages/save_the_date.blade.php
+++ resources/views/pages/save_the_date.blade.php
@@ -6,6 +6,9 @@
             <div class="card shadow-lg card-save-the-date">
                 <div class="card-body">
                     <div class="py-5 p-lg-0">
+
+                        <img src="/images/ornament.png" alt="Ornament" width="100px">
+
                         <h1 class="font-great-vibes display-1 m-0">Save the date</h1>
                         <hr class="mt-0"/>
                         <p class="fw-normal text-muted m-0">21 / 09 / 2024</p>
