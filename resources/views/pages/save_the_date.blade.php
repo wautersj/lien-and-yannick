d444ad827340df365fa55fd14ec96fd71fec8537
Joeri <joeri@digiti.be>
2024-02-04 11:35:08 +0100

    progress

--- resources/views/pages/save_the_date.blade.php
+++ resources/views/pages/save_the_date.blade.php
@@ -1,22 +1,26 @@
 @extends('layout')
 
 @section('content')
-    <div class="container m-auto">
+    <div class="container m-md-auto">
         <div class="block my-4 last">
             <div class="card shadow-lg card-save-the-date">
-                <div class="card-body">
-                    <div class="py-5 p-lg-0">
+                <div>
+                    <div class="card-body">
+                        <div class="py-2 p-lg-0">
 
-                        {{-- <img src="/images/ornament.png" alt="Ornament" width="100px"> --}}
+                            {{-- <img src="/images/ornament.png" alt="Ornament" width="100px"> --}}
 
-                        <h1 class="font-great-vibes display-1 m-0">Save the date</h1>
-                        <hr class="mt-0"/>
-                        <p class="fw-normal text-muted m-0">21 / 09 / 2024</p>
+                            <h1 class="font-great-vibes display-1 m-0">Save the date</h1>
+                            <hr class="mt-0"/>
+                            <p class="fw-normal text-muted m-0">21 / 09 / 2024</p>
+                        </div>
                     </div>
                 </div>
 
-                <div class="img-wrapper">
-                    <img src="{{ Statamic::tag('glide')->params(['src' => $asset, 'width' => 1200, 'format' => 'webp'])->fetch() }}" alt="Thumb">
+                <div>
+                    <div class="img-wrapper">
+                        <img src="{{ Statamic::tag('glide')->params(['src' => $asset, 'width' => 1200, 'format' => 'webp'])->fetch() }}" alt="Thumb">
+                    </div>
                 </div>
             </div>
         </div>
