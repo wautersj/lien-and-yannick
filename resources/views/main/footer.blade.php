5e5053ef1f66be95bf6363296d6ed94bc5de0875
Joeri <joeri@digiti.be>
2024-02-07 00:39:00 +0100

    progress

--- resources/views/main/footer.blade.php
+++ resources/views/main/footer.blade.php
@@ -43,5 +43,7 @@
         </div>
     </div>
 
-    <div class="backgound-image"></div>
+    @if($layout->footer_image)
+        <div class="backgound-image" style="background: url({{ Statamic::tag('glide')->params(['src' => $layout->footer_image, 'width' => 1200, 'height' => 900, 'format' => 'webp'])->fetch() }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);"></div>
+    @endif
 </footer>
