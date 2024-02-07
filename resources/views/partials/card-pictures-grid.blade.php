dc50207ef7af7e70db982ab987bbc315ab2734cc
Joeri <joeri@digiti.be>
2024-02-07 01:24:38 +0100

    progress

--- resources/views/partials/card-pictures-grid.blade.php
+++ resources/views/partials/card-pictures-grid.blade.php
@@ -11,17 +11,17 @@
 <div class="card-pictures-grid">
     <div class="row g-2">
         <div class="col col-7">
-            <div class="picture-wrapper" style="background: url({{ $picture_1 }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
+            <div class="picture-wrapper" style="background: url({{ Statamic::tag('glide')->params(['src' => $picture_1, 'width' => 1000, 'format' => 'webp'])->fetch() }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
                 Asset 1
             </div>
         </div>
         <div class="col col-5">
-            <div class="picture-wrapper" style="background: url({{ $picture_2 }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
+            <div class="picture-wrapper" style="background: url({{ Statamic::tag('glide')->params(['src' => $picture_2, 'width' => 1000, 'format' => 'webp'])->fetch() }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
                 Asset 2
             </div>
         </div>
         <div class="col col-4 d-none d-lg-block">
-            <div class="picture-wrapper" style="background: url({{ $picture_3 }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
+            <div class="picture-wrapper" style="background: url({{ Statamic::tag('glide')->params(['src' => $picture_3, 'width' => 1000, 'format' => 'webp'])->fetch() }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
                 Asset 3
             </div>
         </div>
@@ -31,17 +31,17 @@
             </div>
         </div>
         <div class="col col-4 d-none d-lg-block">
-            <div class="picture-wrapper" style="background: url({{ $picture_4 }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
+            <div class="picture-wrapper" style="background: url({{ Statamic::tag('glide')->params(['src' => $picture_4, 'width' => 1000, 'format' => 'webp'])->fetch() }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
                 Asset 4
             </div>
         </div>
         <div class="col col-5">
-            <div class="picture-wrapper" style="background: url({{ $picture_5 }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
+            <div class="picture-wrapper" style="background: url({{ Statamic::tag('glide')->params(['src' => $picture_5, 'width' => 1000, 'format' => 'webp'])->fetch() }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
                 Asset 5
             </div>
         </div>
         <div class="col col-7">
-            <div class="picture-wrapper" style="background: url({{ $picture_6 }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
+            <div class="picture-wrapper" style="background: url({{ Statamic::tag('glide')->params(['src' => $picture_6, 'width' => 1000, 'format' => 'webp'])->fetch() }}), linear-gradient(60deg, rgb(30, 33, 61) 0%, rgb(76 85 168) 100%);">
                 Asset 6
             </div>
         </div>
