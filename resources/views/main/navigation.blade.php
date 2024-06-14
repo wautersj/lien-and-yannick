064ba068393412f036c06946ae845c1c7ce7ec13
Joeri <joeri@digiti.be>
2024-06-14 10:37:26 +0200

    fe

--- resources/views/main/navigation.blade.php
+++ resources/views/main/navigation.blade.php
@@ -11,16 +11,28 @@
 
                 @foreach (Statamic::tag('nav:main_navigation')->fetch() as $item)
                     <li class="nav-item">
-                        <a class="nav-link
-                            @if($item['is_current']) active @endif
-                        "
-                        @if(($item['new_tab'] ?? null) && $item['new_tab']->value()) target="_blank" @endif
-                        href="{{$item['url']}}">
-                            {{$item['title']}}
-                        </a>
+
+                        {{-- @unless($loop->last) --}}
+
+                            <a class="nav-link
+                                @if($item['is_current']) active @endif
+                            "
+                            @if(($item['new_tab'] ?? null) && $item['new_tab']->value()) target="_blank" @endif
+                            href="{{$item['url']}}">
+                                {{$item['title']}}
+                            </a>
+
+                        {{-- @else
+                            <a href="" class="btn btn-sm btn-primary">
+                                {{$item['title']}}
+                            </a>
+                        @endunless --}}
                     </li>
                 @endforeach
 
+                {{-- <li class="nav-item">
+                    <a href="" class="btn btn-sm btn-secondary">test</a>
+                </li> --}}
             </ul>
         </div>
     </div>
